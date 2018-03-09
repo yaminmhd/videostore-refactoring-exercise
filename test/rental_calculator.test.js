82e60eb96c71c9150021466b6e9da3d7aee4e11d
yaminmhd <mdyamin.drzee@gmail.com>
2018-03-09 15:19:56 +0800

    Restructure project and added src and test folders

--- test/rental_calculator.test.js
+++ test/rental_calculator.test.js
@@ -1,4 +1,4 @@
-const statement = require("./rental_calculator");
+const statement = require("../src/rental_calculator");
 
 const regularMovie = {
   movieID: "F001",
@@ -21,7 +21,7 @@ let movies = {
   [childrenMovie.movieID]: childrenMovie.details
 };
 
-describe("Moive Rental Application", () => {
+describe("Movie Rental Application", () => {
   it("should display customer name in the rental bill", () => {
     let customer = {
       name: "martin",
