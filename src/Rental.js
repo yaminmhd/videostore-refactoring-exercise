765587c61a84775a8c6359335966c376d0386724
yaminmhd <mdyamin.drzee@gmail.com>
2018-03-09 22:03:41 +0800

    Refactored switch block into a method in Rental.js

--- src/Rental.js
+++ src/Rental.js
@@ -3,6 +3,32 @@ class Rental {
     this.movie = movie;
     this.days = days;
   }
+
+  calculateCost(code, days) {
+    let thisAmount = 0;
+
+    // determine amount for each movie
+    switch (code) {
+      case "regular":
+        thisAmount = 2;
+        if (days > 2) {
+          thisAmount += (days - 2) * 1.5;
+        }
+        break;
+      case "new":
+        thisAmount = days * 3;
+        break;
+      case "children":
+        thisAmount = 1.5;
+        if (days > 3) {
+          thisAmount += (days - 3) * 1.5;
+        }
+        break;
+      default:
+        throw new Error("Invalid move type:" + code);
+    }
+    return thisAmount;
+  }
 }
 
 module.exports = Rental;
--- src/rental_calculator.js
+++ src/rental_calculator.js
@@ -1,44 +1,34 @@
+const Customer = require("./Customer");
+const Movie = require("./Movie");
+const Rental = require("./Rental");
+
 module.exports = function statement(customer, movies) {
   let totalAmount = 0;
   let frequentRenterPoints = 0;
   let result = `Rental Record for ${customer.name}\n`;
   for (let r of customer.rentals) {
-    let movie = movies[r.movieID];
-    let thisAmount = 0;
+    let movieTitle = movies[r.movieID].title;
+    let code = movies[r.movieID].code;
 
-    // determine amount for each movie
-    switch (movie.code) {
-      case "regular":
-        thisAmount = 2;
-        if (r.days > 2) {
-          thisAmount += (r.days - 2) * 1.5;
-        }
-        break;
-      case "new":
-        thisAmount = r.days * 3;
-        break;
-      case "children":
-        thisAmount = 1.5;
-        if (r.days > 3) {
-          thisAmount += (r.days - 3) * 1.5;
-        }
-        break;
-      default:
-        throw new Error("Invalid move type:" + movie.code);
+    let movie = new Movie({ id: r.movieID, title: movieTitle, code: code });
+
+    let rental = new Rental(movie, r.days);
+    let cost = rental.calculateCost(movie.code, r.days);
+
+    for (let rental of customer.rentals) {
+      //add frequent renter points
+      frequentRenterPoints++;
+      // add bonus for a two day new release rental
+      if (movie.code === "new" && rental.days > 2) frequentRenterPoints++;
     }
 
-    //add frequent renter points
-    frequentRenterPoints++;
-    // add bonus for a two day new release rental
-    if (movie.code === "new" && r.days > 2) frequentRenterPoints++;
-
     //print figures for this rental
-    result += `\t${movie.title}\t${thisAmount}\n`;
-    totalAmount += thisAmount;
+    result += `\t${movie.title}\t${cost}\n`;
+    totalAmount += cost;
   }
   // add footer lines
   result += `Amount owed is ${totalAmount}\n`;
   result += `You earned ${frequentRenterPoints} frequent renter points\n`;
 
   return result;
-}
+};
