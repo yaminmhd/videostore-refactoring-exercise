50b8100ccbe034a6b2d5ec7aaa47d71bb813d3cc
yaminmhd <mdyamin.drzee@gmail.com>
2018-03-10 11:09:31 +0800

    Separate into 3 separate for loops

--- src/rental_calculator.js
+++ src/rental_calculator.js
@@ -17,33 +17,33 @@ module.exports = function statement(customerRecord, movies) {
       })
   );
 
-  let totalAmount = 0;
-  let frequentRenterPoints = 0;
   let result = `Rental Record for ${customer.name}\n`;
   for (let rental of rentals) {
     let movie = rental.movie;
-
+    let cost = rental.calculateCost();
+    result += `\t${movie.title}\t${cost}\n`;
     // let movieTitle = movies[r.movieID].title;
     // let code = movies[r.movieID].code;
     // let movie = new Movie({ id: r.movieID, title: movieTitle, code: code });
-
     //let rental = new Rental(movie, r.days);
+  }
+
+  let frequentRenterPoints = 0;
+  for (let rental of rentals) {
+    let movie = rental.movie;
+    //add frequent renter points`
+    frequentRenterPoints++;
+    // add bonus for a two day new release rental
+    if (movie.code === "new" && rental.days > 2) frequentRenterPoints++;
+    result += `You earned ${frequentRenterPoints} frequent renter points\n`;
+  }
+
+  //print figures for this rental
+  let totalAmount = 0;
+  for (let rental of rentals) {
     let cost = rental.calculateCost();
-
-    for (let rental of customerRecord.rentals) {
-      //add frequent renter points
-      frequentRenterPoints++;
-      // add bonus for a two day new release rental
-      if (movie.code === "new" && rental.days > 2) frequentRenterPoints++;
-    }
-
-    //print figures for this rental
-    result += `\t${movie.title}\t${cost}\n`;
     totalAmount += cost;
   }
-  // add footer lines
   result += `Amount owed is ${totalAmount}\n`;
-  result += `You earned ${frequentRenterPoints} frequent renter points\n`;
-
   return result;
 };
